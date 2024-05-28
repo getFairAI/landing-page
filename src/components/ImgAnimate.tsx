9f5cdac6a7091219b67a34be376c0b825cc9b604
Eduardo Correia <eduardosareias@gmail.com>
2024-05-28 21:44:36 +0100

    fix: fixed compilation bug

--- src/components/ImgAnimate.tsx
+++ src/components/ImgAnimate.tsx
@@ -16,8 +16,8 @@
  * along with this program. If not, see http://www.gnu.org/licenses/.
  */
 
-import { motion, useAnimation } from 'framer-motion';
-import { useEffect, useRef } from 'react';
+import { motion } from 'framer-motion';
+// import { useEffect, useRef } from 'react';
 
 // const yScrollConfig = -0.5;
 
